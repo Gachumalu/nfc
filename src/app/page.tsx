d08008dde118cd525faa40b1191179e9a05b2a14
godson malu <godsonm2000@gmail.com>
2024-08-07 12:14:09 -0500

    4 commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,42 @@
 import Link from "next/link";
 import MatchPages from "./components/MatchPages";
 import { match1, match2 } from "@/data/data";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <>
-      <div className=" bg-yellows h-screen pt-36 ">
-        <Link
-          href={{
-            pathname: "/matchdetails",
-            query: { ...match1 },
-          }}
-        >
-          <MatchPages
-            team1Name="DFC"
-            team2Name="STC"
-            team1Image="godson.png"
-            team2Image="godson.png"
-          />
-        </Link>
+      <Suspense>
+        <div className=" bg-yellows h-screen pt-36 ">
+          <Link
+            href={{
+              pathname: "/matchdetails",
+              query: { ...match1 },
+            }}
+          >
+            <MatchPages
+              team1Name="DFC"
+              team2Name="STC"
+              team1Image="godson.png"
+              team2Image="godson.png"
+            />
+          </Link>
 
-        <Link
-          href={{
-            pathname: "/matchdetails",
-            query: { ...match2 },
-          }}
-        >
-          <MatchPages
-            team1Name="DBU"
-            team2Name="ST.JUDE"
-            team1Image="godson.png"
-            team2Image="godson.png"
-          />
-        </Link>
-      </div>
+          <Link
+            href={{
+              pathname: "/matchdetails",
+              query: { ...match2 },
+            }}
+          >
+            <MatchPages
+              team1Name="DBU"
+              team2Name="ST.JUDE"
+              team1Image="godson.png"
+              team2Image="godson.png"
+            />
+          </Link>
+        </div>
+      </Suspense>
     </>
   );
 }
